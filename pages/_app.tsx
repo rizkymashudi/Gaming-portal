4a6ad93e84d28378bb0ce474c8b025b9ea27b224
rizkym <rizkymashudi7@gmail.com>
2022-06-21 21:56:13 +0700

    Create checkout page
    Convert checkout plain html into react component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import '../styles/utilities.css';
 import '../styles/homepage.css';
 import '../styles/detail.css';
+import '../styles/checkout.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import detail from './detail/index';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
