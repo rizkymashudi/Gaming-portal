fabf8ed5bc6f5080453235a5464b0064cc62f213
rizkym <rizkymashudi7@gmail.com>
2022-06-21 22:08:58 +0700

    Create sign in page
    Convert Sign In page plain html into react component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,9 @@ import '../styles/homepage.css';
 import '../styles/detail.css';
 import '../styles/checkout.css';
 import '../styles/complete-checkout.css';
+import '../styles/sign-in.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import checkout from './checkout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
